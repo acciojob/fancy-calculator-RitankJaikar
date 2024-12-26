c1c7afafe67aba3ed9f5bd3c45a9adf1c050719d
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-12-26 10:03:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+// Take input for the first variable
+const firstInput = parseInt(prompt("Enter the first number:"));
+
+// Multiply the first input by 2
+const multipliedResult = firstInput * 2;
+
+// Take input for the second variable
+const secondInput = parseInt(prompt("Enter the second number:"));
+
+// Add the second input to the result of the first input multiplied by 2
+const finalResult = multipliedResult + secondInput;
+
+// Display the final result in the specified format
+alert("The final result is: " + finalResult);
